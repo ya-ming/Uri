5e044fa5712bcf93b659f1dc2e5cd8f2b1859e72
YaMing Wu <wuyaming@gmail.com>
2019-05-07 21:05:46 -0500

    Refactoring
    * Add unit tests for stand-alone modules.
    * CharacterSet, NormalizeCaseInsensitiveString and PercentEncodedCharacterDecoder.

---
+++ test/src/CharacterSetTests.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file NormalizeCaseInsensitiveStringTests.cpp
+ *
+ * This module contains the unit tests of the Uri::NormalizeCaseInsensitiveStringTests class.
+ *
+ * © 2019 YaMing Wu
+ */
+
+#include <gtest/gtest.h>
+#include <src/NormalizeCaseInsensitiveString.hpp>
+
+ TEST(NormalizeCaseInsensitiveStringTests, NormalizeCaseInsensitiveString) {
+    ASSERT_EQ(
+        "example",
+        Uri::NormalizeCaseInsensitiveString("eXAmplE")
+    );
+    ASSERT_EQ(
+        "example",
+        Uri::NormalizeCaseInsensitiveString("example")
+    );
+    ASSERT_EQ(
+        "example",
+        Uri::NormalizeCaseInsensitiveString("EXAMPLE")
+    );
+    ASSERT_EQ(
+        "foo1bar",
+        Uri::NormalizeCaseInsensitiveString("foo1BAR")
+    );
+    ASSERT_EQ(
+        "foo1bar",
+        Uri::NormalizeCaseInsensitiveString("fOo1bAr")
+    );
+    ASSERT_EQ(
+        "foo1bar",
+        Uri::NormalizeCaseInsensitiveString("foo1bar")
+    );
+    ASSERT_EQ(
+        "foo1bar",
+        Uri::NormalizeCaseInsensitiveString("FOO1BAR")
+    );
+}
